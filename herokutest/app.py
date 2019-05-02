f6b8a6e570f53bfdf3cde1a3f6caccf517c9273c
Sarah Marquardt <smarquardtcwp@gmail.com>
2019-05-02 13:10:10 -0500

    adding smaller model for Heroku + slight html changes

--- herokutest/app.py
+++ herokutest/app.py
@@ -1,5 +1,3 @@
-
-
 import os
 from flask import Flask, request, render_template, redirect, jsonify
 from flask_mysqldb import MySQL
@@ -7,13 +5,14 @@ import pickle
 mysql = MySQL()
 from time import sleep
 from ML import classify
+
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'ju78b2yixa3evno4'
 app.config['MYSQL_PASSWORD'] = 'cqt4d0xvnqbtz1qt'
 app.config['MYSQL_DB'] = 'mnmneqbmhy3vmj37'
 app.config['MYSQL_HOST'] = 'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
-#mysql.init_app(app)
 mysql = MySQL(app)
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -25,28 +24,14 @@ def upload_file():
             filename = file.filename 
             file.save(os.path.join("uploads/", filename))
             predict=classify.classifier(f"uploads/{filename}")
-            # str1 = ''.join(predict)
-            # print(str1)
-            # c1 = str1.split(":")
 
             cur = mysql.connection.cursor()
-            #var = "SELECT * FROM mnmneqbmhy3vmj37.cocktails1 where Ingredients Like '%"+predict+"%';"
-            #var = ("SELECT Drink, Ingredients from mnmneqbmhy3vmj37.cocktails1 where Ingredients Like '%"+predict+"%';")
-            #cur.execute(var)
             
             cur.execute("SELECT Drink, Ingredients, Instructions from mnmneqbmhy3vmj37.cocktails1 where Ingredients Like '%"+predict+"%';")
-            #cur.execute("SELECT * From allrecipes WHERE Ingredients LIKE '%Apple%'")
             data = cur.fetchall()
-           
-            #print(var)
-        
-            return render_template('index.html', object_list=data)
-            #other_data= jsonify({'data': 'data'})
-    
+            return render_template('index.html', object_list=data, predict=predict)    
        
-    return render_template('index.html')  
- 
-    
+    return render_template('index.html')   
             
 if __name__ == "__main__":
     app.run(debug=True)
